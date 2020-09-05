49afd5f8f1714f0047112999abe61ec73a7b0d8f
Ben Haus <ben@little-haus.com>
2020-09-05 14:35:48 -0400

    updated app to support video files

--- src/components/APOD.jsx
+++ src/components/APOD.jsx
@@ -10,6 +10,10 @@ const SpacePic = styled.img`
   width: 100%;
   height: auto;
 `;
+const SpaceVid = styled.iframe`
+  width: 100%;
+  height: auto;
+`;
 const APODContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -25,7 +29,7 @@ const ContentContainer = styled.section`
   padding: 2%;
   margin-bottom: 15px;
   border: 2px solid black;
-  background: ${props => (props.primary ? "lightblue" : "lightgrey")};
+  background: ${(props) => (props.primary ? "lightblue" : "lightgrey")};
   color: black;
   border-radius: 20px;
 `;
@@ -37,7 +41,7 @@ export default function APOD() {
   const [dateURL, setDateURL] = useState("");
 
   //Fcn to set up date picker
-  const archiveURL = date => {
+  const archiveURL = (date) => {
     return setDateURL(`&date=${date}`);
   };
 
@@ -46,39 +50,70 @@ export default function APOD() {
       .get(
         `https://api.nasa.gov/planetary/apod?api_key=Fxx4K3wvOl3k3rZ7xqXhJk3bCwLtfxbMXnbAbhLA${dateURL}`
       )
-      .then(response => {
+      .then((response) => {
         // console.log(response.data);
         setPOD(response.data);
       })
-      .catch(error => console.log("the NASA APOD was not returned", error));
+      .catch((error) => console.log("the NASA APOD was not returned", error));
   }, [dateURL]);
 
   //Loading message for slow-loads
-  if (!POD) return <h3>Loading...</h3>;
-
-  return (
-    <APODContainer>
-      <ContentContainer>
-        <a href={POD.hdurl} target="_blank" rel="noopener noreferrer">
-          <figure>
-            <SpacePic
+  if (!POD) {
+    return <h3>Loading...</h3>;
+  } else if (POD.media_type === "image") {
+    return (
+      <APODContainer>
+        <ContentContainer>
+          <a href={POD.hdurl} target="_blank" rel="noopener noreferrer">
+            <figure>
+              <SpacePic
+                src={POD.url}
+                alt="the NASA Astronomy Pic of the Day"
+                title={POD.title}
+              />
+              <figcaption>
+                {`Copyright: ${POD.copyright} | Date: ${POD.date}`}
+              </figcaption>
+              <figcaption>(click for hi-res version)</figcaption>
+            </figure>
+          </a>
+        </ContentContainer>
+        <ContentContainer primary>
+          <Facts data={POD} />
+        </ContentContainer>
+        <ContentContainer>
+          <ArchiveAPOD archiveURL={archiveURL} />
+        </ContentContainer>
+      </APODContainer>
+    );
+  } else if (POD.media_type === "video") {
+    console.log(POD);
+    return (
+      <APODContainer>
+        <ContentContainer>
+          <a href={POD.url} target="_blank" rel="noopener noreferrer">
+            <figure>
+              <SpaceVid
               src={POD.url}
-              alt="the NASA Astronomy Pic of the Day"
+              alt="the NASA Astronomy Vid of the Day"
               title={POD.title}
-            />
-            <figcaption>
-              {`Copyright: ${POD.copyright} | Date: ${POD.date}`}
-            </figcaption>
-            <figcaption>(click for hi-res version)</figcaption>
-          </figure>
-        </a>
-      </ContentContainer>
-      <ContentContainer primary>
-        <Facts data={POD} />
-      </ContentContainer>
-      <ContentContainer>
-        <ArchiveAPOD archiveURL={archiveURL} />
-      </ContentContainer>
-    </APODContainer>
-  );
+              />
+              <figcaption>
+                {`Copyright: ${POD.copyright} | Date: ${POD.date}`}
+              </figcaption>
+              <figcaption>(click for external video)</figcaption>
+            </figure>
+          </a>
+        </ContentContainer>
+        <ContentContainer primary>
+          <Facts data={POD} />
+        </ContentContainer>
+        <ContentContainer>
+          <ArchiveAPOD archiveURL={archiveURL} />
+        </ContentContainer>
+      </APODContainer>
+    );
+  } else {
+    return <h3>Sorry, today's APOD file type not supported.</h3>;
+  }
 }
